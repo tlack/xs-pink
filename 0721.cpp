1c285c69b6b6cbd25c63570877618cd306df7ad5
tlack <lackner@gmail.com>
2018-07-24 00:07:47 -0400

    over() and scan() and lotsalotsa tests

--- 0721.cpp
+++ 0721.cpp
@@ -1,5 +1,5 @@
 /* 0721: embrace methods */
-
+#include <math.h>
 #include <cstdarg>
 #include <iostream>
 #include <stdio.h>
@@ -11,6 +11,7 @@ using OS=std::ostream;
 TMPL(Tx) void emit(Tx x,const char* lbl) { std::cout<<lbl<<": "<<x<<"\n"; }
 void emit(const char* s) { std::cout<<s<<"\n"; }
 void emit(int i) { std::cout<<i; }
+void emit(double i) { std::cout<<i; }
 int max(int a,int b) { return a>b?a:b; }
 TMPL(Tx) void _test(const char* n, Tx got, Tx expected) {
 	if(got==expected) emit(n,"passed");
@@ -58,6 +59,7 @@ void test_xsym() {
 TMPL(Tx) class Xvec;
 TMPL(Tx) class Xiter : public Xany {
 	public:
+	virtual ~Xiter(){};
 	virtual Tx operator[](int n)=0;
 	virtual int find(Tx x)=0;
 	virtual bool has(Tx x) { return find(x)!=-1; }
@@ -68,6 +70,7 @@ TMPL(Tx) class Xiter : public Xany {
 };
 TMPL(Tx) Tx empty(Xiter<Tx>& x) { return x._len==0; }
 TMPL(Tx) Tx get(Xiter<Tx>& x, int n) { return x[n]; }
+TMPL(Tx) Tx get(Xiter<Tx>* x, int n) { return (*x)[n]; }
 TMPL(Tx) int len(Xiter<Tx>& x) { return x.len(); }
 
 class Xrange : public Xiter<int> {
@@ -96,6 +99,7 @@ TMPL(Tx) class Xvec : public Xiter<Tx> {
 	public:
 	Xvec(int n_) : data(new Tx[n_]),n(n_),_len(0) {};
 	Xvec(int n_,const Tx x[]) : n(n_),data(new Tx[n_]),_len(n_) { emit(n_); for(int i=0;i<n_;i++)data[i]=x[i]; };
+	~Xvec() { delete data; }
 	Tx operator[](int i) {return data[i];}
 	void amend(Xvec<int> x,Xvec<Tx> y) { for(int i=0;i<x._len;i++) amend(x[i],y[i]); };
 	void amend(int i,Tx y) { if(i>-1 && i<_len) data[i]=y; };
@@ -103,32 +107,31 @@ TMPL(Tx) class Xvec : public Xiter<Tx> {
 	int insert(Tx x) { if (_len>=n) return -1; data[_len++]=x; return _len-1; }
 	int len() { return _len; }
 	void repr(OS& o) { 
-		o<<tag<<"("<<_len<<",("; 
+		o<<"vec("<<_len<<",("; 
 		for(int i=0;i<_len;i++) { o<<data[i]; if(i<_len-1) o<<","; } o<<"))"; }
-	int _len; 
-	Tx* data; int n; 
-	char* tag="vec";
+	int _len; Tx* data; int n; 
 };
+typedef Xvec<int> Xint;
+typedef Xvec<char> Xstr;
 TMPL(Tx) void amend(Xvec<Tx>& x, int i, Tx y) { x.amend(i,y); }
 TMPL(Tx) void amend(Xvec<Tx>& x, Xvec<int> i, Xvec<Tx> y) { x.amend(i,y); }
 TMPL(Tx) Xvec<Tx>* each(Xiter<Tx>& x, Tx (*cb)(Tx,int)){ 
-	//emit(x,"each a");
-	emit("each a:");
-	x.repr(std::cout);
-	emit(x.len(),"each b");
-	int n=x.len(),i=0; auto R=new Xvec<Tx>(n); for(;i<n;i++)insert(R,cb(x[i],i)); 
-	emit(i,"done");
-	return R; }
-TMPL(Tx) Xvec<Tx>* each(Xiter<Tx>& x, Xvec<Tx>* (*cb)(Xiter<Tx>&)) { 
-	return cb(x); }
+	int n=x.len(),i=0; auto R=new Xvec<Tx>(n); 
+	for(;i<n;i++)insert(R,cb(x[i],i)); return R; }
+TMPL(Tx) Xvec<Tx>* each(Xiter<Tx>& x, Xvec<Tx>* (*cb)(Xiter<Tx>&)) { return cb(x); }
 TMPL(Tx) Xvec<Tx>* get(Xiter<Tx>& x, Xvec<Tx>*y) { 
 	int n=y.len(),i; auto R=new Xvec<Tx>(n);
 	for(i=0;i<n;i++)insert(R,x[y[i]]); return R; }
 TMPL(Tx) int insert(Xvec<Tx>& x, Tx y) { return x.insert(y); }
 TMPL(Tx) int insert(Xvec<Tx>* x, Tx y) { return x->insert(y); }
 TMPL(Tx) int len(Xvec<Tx>& x) { return x.len(); }
-typedef Xvec<int> Xint;
-typedef Xvec<char> Xstr;
+TMPL(Tx) Tx over(Xiter<Tx>& x, Tx(*cb)(Tx,Tx)) {
+	if(empty(x)) throw XLengthErr();
+	Tx r=x[0]; int n=len(x),i; for(i=1;i<n;i++)r=cb(r,x[i]); return r;  }
+TMPL(Tx) Xvec<Tx>* scan(Xiter<Tx>& x, Tx(*cb)(Tx,Tx)) {
+	if(empty(x)) throw XLengthErr();
+	int n=len(x),i; Tx last; auto r=new Xvec<Tx>(n); 
+	insert(r,last=x[0]); for(i=1;i<n;i++)insert(r,last=cb(last,x[i])); return r;  }
 
 double test_xvec_f1(double x, int i) {
 	//emit(x);
@@ -141,52 +144,41 @@ Xvec<double>* test_xvec_f2(Xiter<double>& x) {
 	for(int i=0;i<len(x);i++) insert(r,x[i]*i);
 	return r;
 }
+double test_xvec_f3(double x,double y) { emit(x,"f3/1"); emit(y,"f3/2"); return x*y; }
 void test_xvec() {
 	emit("test(xvec)");
 	Xvec<int> v1(2);
-	_test("xvec 1",v1.n,2);
-	_test("xvec 2",v1._len,0);
+	_test("xvec 1",v1.n,2); _test("xvec 2",v1._len,0);
 	v1.insert(9);
-	_test("xvec 3",v1._len,1);
-	_test("xvec 4",v1.data[0],9);
-	_test("xvec 5",v1.n,2);
+	_test("xvec 3",v1._len,1); _test("xvec 4",v1.data[0],9); _test("xvec 5",v1.n,2);
 	insert(v1,10);
-	_test("xvec 6",v1._len,2);
-	_test("xvec 7",v1.data[v1._len-1],10);
+	_test("xvec 6",v1._len,2); _test("xvec 7",v1.data[v1._len-1],10);
 	Xstr v2(5,"Hello");
-	v2.amend(1,'x');
-	_test("xvec amend 1",v2.data[1],'x');
-	int i2[]={0,1};
-	int i3[]={55,66};
-	v1.amend(Xvec<int>(2,i2),Xvec<int>(2,i3));
-	_test("xvec amend 2",v1[0],55);
-	_test("xvec amend 3",v1[1],66);
-
+	v2.amend(1,'x'); _test("xvec amend 1",v2.data[1],'x');
+	int i2[]={0,1}; int i3[]={55,66}; v1.amend(Xvec<int>(2,i2),Xvec<int>(2,i3));
+	_test("xvec amend 2",v1[0],55); _test("xvec amend 3",v1[1],66);
 	Xvec<double> v3(3);
 	insert(v3,10.10); insert(v3,20.20); insert(v3,30.30);
-	_test("xvec double 1",v3[0],10.10);
-	_test("xvec double _len",v3._len,3);
-
-	emit(v3);
+	_test("xvec double 1",v3[0],10.10); _test("xvec double _len",v3._len,3);
 	Xvec<double>* v4=each(v3,test_xvec_f1);
-	emit(*v4);
-	_test("xvec each 1",v4->_len,v3._len);
-	_test("xvec each 2",v4->data[0],0.0);
-	_test("xvec each 3",v4->data[1],20.20);
-	_test("xvec each 4",v4->data[2],60.60);
+	_test("xvec each 1",v4->_len,v3._len); _test("xvec each 2",v4->data[0],0.0);
+	_test("xvec each 3",v4->data[1],20.20); _test("xvec each 4",v4->data[2],60.60);
 	delete v4;
 	auto v5=each(v3,test_xvec_f2);
-	_test("xvec each/all 1",v5->_len,v3._len);
-	_test("xvec each/all 2",v5->data[0],0.0);
-	_test("xvec each/all 3",v5->data[1],20.20);
-	_test("xvec each/all 4",v5->data[2],60.60);
+	_test("xvec each/all 1",v5->_len,v3._len); _test("xvec each/all 2",v5->data[0],0.0);
+	_test("xvec each/all 3",v5->data[1],20.20); _test("xvec each/all 4",v5->data[2],60.60);
 	delete v5;
+	auto v6=over(v3,test_xvec_f3); _test("xvec over double",abs(v6),6181);
+	Xvec<double>* v7=scan(v3,test_xvec_f3);
+	_test("xvec scan/1",abs((*v7)[0]),10); _test("xvec scan/2",abs((*v7)[1]),204);
+	_test("xvec scan/3",abs((*v7)[2]),6181); _test("xvec scan/4",len(v7),3);
 }
 template <typename Tk,typename Tx> class Xmap : public Xiter<Tx> {
 	public:
 	Xmap(int n_) : key(n_),data(n_),n(n_),_len(0) {}; 
 	Xmap(int n_, Tk k[], Tx x[]) : key(n_),data(n_),n(n_),_len(0) {
 		for(int i=0;i<n_;i++) { key.insert(k[i]); data.insert(x[i]); } _len=n_; }
+	//~Xmap(){delete key; delete data;}
 	Tx operator[](Tk i) { return data[key.find(i)]; }
 	Tx operator[](Tk* i) { return data[key.find(*i)]; }
 	Tx operator[](int i) { return data[i]; }
@@ -201,14 +193,14 @@ template <typename Tk,typename Tx> class Xmap : public Xiter<Tx> {
 		if (_len>=n) return -1; key.insert(x); data.insert(y); 
 		return _len++; }
 	int insert(Tk* x,Tx y) { return insert(*x,y); }
-	void repr(OS& o) { o << tag << "(" << _len << "," << key << "," << data << ")"; }
+	void repr(OS& o) { o << "map(" << _len << "," << key << "," << data << ")"; }
 	Xvec<Tk> key; Xvec<Tx> data; int n,_len; char* tag="map"; };
 TMPL(Tx) class Xdict : public Xmap<Xsym,Tx> {
 	public:
 	typedef Xmap<Xsym,Tx> super;
 	Xdict(int n):Xmap<Xsym,Tx>(n){};
 	Xdict(int n,Xsym k[],Tx x[]):Xmap<Xsym,Tx>(n,k,x){};
-	void repr(OS& o) { o << tag << "(" << super::_len << "," << super::key << "," << super::data << ")"; }
+	void repr(OS& o) { o << "dict(" << super::_len << "," << super::key << "," << super::data << ")"; }
 	char* tag="dict";
 };
 
